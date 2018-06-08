50698e5f6b87abff712c8a0a0e03bd9c416459f7
Boris Däppen <bdaeppen.perl@gmail.com>
2018-06-08 10:28:42 +0200

    v0.002 - added stub module for cpan indexing

---
+++ lib/App/Unding.pm
@@ -0,0 +1,26 @@
+package App::Unding;
+
+use strict;
+use warnings;
+# Autor: Boris Däppen, 2018
+# No guarantee given, use at own risk and will
+
+# ABSTRACT: dark magic, encrypted wallet
+
+use v5.6.0;
+
+# THIS IS A STUB MODULE FOR CPAN INDEXING
+# https://www.perl.com/article/how-to-upload-a-script-to-cpan/
+
+__END__
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+This is a stub module for cpan indexing.
+No funcionality implemented.
+See L<https://www.perl.com/article/how-to-upload-a-script-to-cpan/> for the reason.
+
+You find the implementation in the script L<unding>.
+
